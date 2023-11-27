94dded60ecd1ef38f81a2433523ebd4f2e2954ee
RSainarayan <shivank2002@gmail.com>
2023-11-27 22:56:57 +0530

    latest update without redis

--- Js/index.js
+++ Js/index.js
@@ -1,49 +1,49 @@
 $(document).ready(
-     function ajaxCall() {
-    
-    var session = localStorage.getItem('session');
-    var expireTime = localStorage.getItem('expireTime');
-    var currentTime = new Date().getTime();
+    function ajaxCall() {
+   
+   var username = localStorage.getItem('username');
+   var expireTime = localStorage.getItem('expireTime');
+   var currentTime = new Date().getTime();
 
-    if (session && expireTime && currentTime < expireTime) {
+   if (username && expireTime && currentTime < expireTime) {
        
-        window.location.href = 'http://localhost:5500/Style/home.html';
-    }
+       window.location.href = 'http://localhost:5500/Style/home.html';
+   }
 
-    
-    $("#loginForm").submit(function(event) {
-        event.preventDefault(); 
+   
+   $("#loginForm").submit(function(event) {
+       event.preventDefault(); 
 
-        
-        var formData = $(this).serialize();
+      
+       var formData = $(this).serialize();
 
-        
-        $.ajax({
-            url: 'http://localhost/Loginform/php/login.php',
-            type: 'POST',
-            data: formData,
-            dataType: 'json', 
-            success: function(response) {
-                
-        
-                if (response.status === 'success') {
-                    alert('Logged: ' + response.status);  
-                    var username = $('#username').val();
-                    var expireTime = new Date();
-                    expireTime.setMinutes(expireTime.getMinutes() + 1); 
-                    localStorage.setItem('session', response.session);
-                    localStorage.setItem('expireTime', expireTime.getTime()+60);
-                    window.location.href = 'http://localhost:5500/Style/home.html';
-                } else {
-                  
-                    alert('Error: ' + response.message);
-                }
-            },
-            error: function(xhr, status, error) {
-              
-                console.error('Error:', status, error);
-            }
-        });
-        
-    }); 
+       
+       $.ajax({
+           url: 'http://localhost/Loginform/php/login.php',
+           type: 'POST',
+           data: formData,
+           dataType: 'json', 
+           success: function(response) {
+               
+       
+               if (response.status === 'success') {
+                   alert('Logged: ' + response.status);  
+                   var username = $('#username').val();
+                   var expireTime = new Date();
+                   expireTime.setMinutes(expireTime.getMinutes() + 1); // Example: session expires in 30 minutes
+                   localStorage.setItem('username', username);
+                   localStorage.setItem('expireTime', expireTime.getTime());
+                   window.location.href = 'http://localhost:5500/Style/home.html';
+               } else {
+                   
+                   alert('Error: ' + response.message);
+               }
+           },
+           error: function(xhr, status, error) {
+               
+               console.error('Error:', status, error);
+           }
+       });
+       
+   }); 
 }); 
--- php/login.php
+++ php/login.php
@@ -1,15 +1,7 @@
 <?php
-
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
-$redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
-
-if (!$redis->ping()) {
-    die("Redis server is not running");
-}
 
 $servername = "localhost";
 $username = "root";
@@ -26,52 +18,45 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     exit;
 }
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $loginUsername = $_POST["username"];
     $loginPassword = $_POST["password"];
 
+    
     $stmt = $conn->prepare("SELECT username, password FROM users WHERE username = ?");
     $stmt->bind_param("s", $loginUsername);
     $stmt->execute();
     $stmt->store_result();
-    $cacheKey = "user:$email:password:$hashedPassword";
-
-    if ($redis->exists($cacheKey)) {
-        echo "success (from cache)";
-    }else{
-        if ($stmt->num_rows > 0) {
-            $stmt->bind_result($dbUsername, $dbPassword);
-            $stmt->fetch();
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($dbUsername, $dbPassword);
+        $stmt->fetch();
+    
         
+        if (password_verify($loginPassword, $dbPassword)) {
             
-            if (password_verify($loginPassword, $dbPassword)) {
-                $redis->set($cacheKey, 1);
-                $redis->expire($cacheKey, 60); 
-                $response = [
-                    'status' => 'success',
-                    'message' => 'Login successful',
-                    'session'=>$cacheKey
-                ];
-            } else {
-                
-                $response = [
-                    'status' => 'error',
-                    'message' => 'Incorrect password',
-                ];
-            }
+            $response = [
+                'status' => 'success',
+                'message' => 'Login successful',
+            ];
         } else {
             
             $response = [
                 'status' => 'error',
-                'message' => 'Username not found',
+                'message' => 'Incorrect password',
             ];
         }
+    } else {
+        
+        $response = [
+            'status' => 'error',
+            'message' => 'Username not found',
+        ];
     }
-   
     
     $stmt->close();
     
-   
+    
     header('Content-Type: application/json');
     echo json_encode($response);
     exit;
